5509f96769ae3d160038a9f0823c1bff0930f40d
Axel Manguian <axel.manguian@epitech.eu>
2025-10-06 21:11:41 +0200

    feat(frontend): initialize Next.js + TypeScript project setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">Welcome to Horas</h1>
+    </main>
+  );
+}
