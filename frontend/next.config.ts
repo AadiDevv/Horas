4754ab441947c0d4a5b036e76db0903cdc632886
Axel Manguian <axel.manguian@epitech.eu>
2025-10-07 12:09:07 +0200

    feat: add next config file and login folder

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  poweredByHeader: false, // Don't reveal that the app is powered by Next.js
+  compiler: { removeConsole: process.env.NODE_ENV === "production" }, // Remove console logs in production
 };
 
 export default nextConfig;
