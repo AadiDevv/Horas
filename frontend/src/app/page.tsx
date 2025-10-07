eeb8dcd165c146297ce161cf6efdbed0e8564a60
Adry <adrien.mery-costa@epitech.eu>
2025-10-07 16:53:12 +0200

    feat: add-landing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 export default function Home() {
   return (
-    <main className="flex h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold">Welcome to Horas</h1>
-    </main>
+    <div className="bg-white min-h-screen font-sans">
+      <h1>Welcome to Horas</h1>
+    </div>
   );
 }
