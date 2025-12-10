3be2935daaee06857bcd2e06c450a3b391305b59
16ur <akuseruuu@protonmail.com>
2025-12-10 19:47:45 +0100

    Revert "fix(user): redirect the user to adapted dashboard when already connected"
    This reverts commit 0b698f5f662e543c0724a0caa18c8f1a67009a15.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 import Navbar from './components/navbar';
 import Hero from './components/herobanner';
 import Feature from './components/featuresection';
@@ -12,23 +10,6 @@ import Footer from './components/footer';
 
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    const role = localStorage.getItem('role');
-
-    if (token && role) {
-      let redirectPath = '/home';
-      if (role === 'employe') {
-        redirectPath = '/dashboard-agent';
-      } else if (role === 'manager') {
-        redirectPath = '/dashboard-manager';
-      }
-      router.push(redirectPath);
-    }
-  }, [router]);
-
   return (
     <div className="bg-white min-h-screen font-sans">
       <Navbar />
